2a75c702de73f2a1d857b054cc73ed4061e6e579
luyang <643401727@qq.com>
2016-11-21 15:19:16 +0800

    初始化项目

---
+++ netgame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  netgame
+//
+//  Created by luyang on 2016/11/21.
+//  Copyright © 2016年 luyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
